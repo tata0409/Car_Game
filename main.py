d10233f49a228a8c043b2db49f58a2fd192b9d8e
tata0409 <tatachechyna@gmail.com>
2025-02-22 01:21:06 -0500

    Car can't go beyond the screen

--- main.py
+++ main.py
@@ -44,6 +44,18 @@ parking_img = images_dict['taxi_background']
 parking_rect = parking_img.get_rect()
 parking_rect.x, parking_rect.y = hotel_rect.x, hotel_rect.y + hotel_rect.height
 
+def is_crash():
+    for x in range(player_rect.x, player_rect.topright[0], 1):
+        for y in range(player_rect.y, player_rect.bottomleft[1], 1):
+            try:
+                if screen.get_at((x,y)) == (220, 215, 177):
+                    return True
+            except:
+                print("Oops")
+    if hotel_rect.colliderect(player_rect):
+        return True
+    return False
+
 pg.init()
 screen = pg.display.set_mode([width, height])
 timer = pg.time.Clock()
@@ -56,16 +68,16 @@ while run:
             run = False
 
     keys_klava = pg.key.get_pressed()
-    if keys_klava[pg.K_RIGHT]:
+    if keys_klava[pg.K_RIGHT] and player_rect.x < width - player_rect.width:
         x_direction = 1
         player_view = 'right'
-    elif keys_klava[pg.K_LEFT]:
+    elif keys_klava[pg.K_LEFT] and player_rect.x > 0:
         x_direction = -1
         player_view = 'left'
-    elif keys_klava[pg.K_UP]:
+    elif keys_klava[pg.K_UP] and player_rect.y > 0:
         y_direction = -1
         player_view = 'rear'
-    elif keys_klava[pg.K_DOWN]:
+    elif keys_klava[pg.K_DOWN] and player_rect.y <= height - player_rect.height:
         y_direction = 1
         player_view = 'front'
 
@@ -74,12 +86,15 @@ while run:
     x_direction = 0
     y_direction = 0
 
+    if is_crash():
+        print("CRASH")
+
     screen.fill(BLACK)
     screen.blit(images_dict['bg'], (0, 0))
 
-    screen.blit(images_dict['taxi_background'], parking_rect)
-    screen.blit(images_dict['hotel'], hotel_rect)
-    screen.blit(images_dict['passenger'], passenger_rect)
+    screen.blit(parking_img, parking_rect)
+    screen.blit(hotel_img, hotel_rect)
+    screen.blit(passenger_img, passenger_rect)
     screen.blit(images_dict['player'][player_view], player_rect)
 
     pg.display.flip()
