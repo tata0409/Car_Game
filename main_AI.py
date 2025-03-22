cca4fd0de1e937f6fe1727d354038ef543c279f7
tata0409 <tatachechyna@gmail.com>
2025-03-22 01:50:56 -0400

    Main learning cycle added

--- main_AI.py
+++ main_AI.py
@@ -1,11 +1,13 @@
+from collections import defaultdict
+import numpy as np
 import pygame as pg
 import random
-import numpy as np
-from collections import defaultdict
 
 width, height = 700, 450
 FPS = 60
+
 BLACK = (0, 0, 0)
+
 x_direction = 0
 y_direction = 0
 player_speed = 2
@@ -39,90 +41,56 @@ hotel_positions = [
 ]
 hotel_rect.x, hotel_rect.y = random.choice(hotel_positions)
 
+
+parking_img = images_dict['parking']
+parking_rect = parking_img.get_rect()
+parking_rect.x, parking_rect.y = hotel_rect.x, hotel_rect.y + hotel_rect.height
+
+
 passenger_img = images_dict['passenger']
 passenger_rect = passenger_img.get_rect()
 passenger_rect.x, passenger_rect.y = random.choice(hotel_positions)
 while passenger_rect.x == hotel_rect.x and passenger_rect.y == hotel_rect.y:
     passenger_rect.x, passenger_rect.y = random.choice(hotel_positions)
 passenger_rect.y += hotel_rect.height
-pas_position = passenger_rect.x, passenger_rect.y
-passenger_picked = False
 
-parking_img = images_dict['parking']
-parking_rect = parking_img.get_rect()
-parking_rect.x, parking_rect.y = hotel_rect.x, hotel_rect.y + hotel_rect.height
-
-##################
-actions = [0, 1, 2, 3]
-Q_table = defaultdict(lambda: [0 ,0 ,0 ,0])
-learning_rate = 0.9
-discount_factor = 0.9
-epsilon = 0.1
-
-
-def update_Q(state, action, reward,next_state):
-    best_next = max(Q_table(next_state))
-    Q_table[state][action] += learning_rate * (reward + discount_factor * best_next - Q_table[state][action])
-
-
-def make_step():
-    current_state = (player_rect.x, player_rect.y)
-    action = choose_action(current_state)
-    apply_action(action)
-    reward = -1
-    episode_end = False
-    success = False
-    if is_crash():
-        reward = -100
-        episode_end = True
-    if parking_rect.contains((player_rect)):
-        reward = 100
-        success = True
-    next_state = (player_rect.x, player_rect.y)
-    update_Q(current_state, action, reward, next_state)
-    return (episode_end, success)
-
-
-
-def choose_action(state):
-    if random.random()<epsilon:
-        return random.choice(actions)
-    else:
-        return np.argmax(Q_table(state))
-
-##################
+pg.init()
+screen = pg.display.set_mode([width, height])
 
 
 def apply_action(action):
+    if action is None:
+        return
     global player_view, x_direction, y_direction
     x_direction, y_direction = 0, 0
-    if action == 0:
+    if action == 0 and player_rect.x <= width - player_rect.width:
         x_direction = 1
         player_view = 'right'
-    elif action == 1:
+    elif action == 1 and player_rect.x > 0:
         x_direction = -1
         player_view = 'left'
-    elif action == 2:
+    elif action == 2 and player_rect.y > 0:
         y_direction = -1
         player_view = 'rear'
-    elif action == 3:
+    elif action == 3 and player_rect.y <= height - player_rect.height:
         y_direction = 1
         player_view = 'front'
+
+    new_x = player_rect.x + player_rect.width * x_direction
+    new_y = player_rect.y + player_rect.height * y_direction
+    if 0 < new_x <= width - player_rect.width and 0 < new_y <= height - player_rect.height:
+        player_rect.x, player_rect.y = new_x, new_y
 
 
 def is_crash():
     for x in range(player_rect.x, player_rect.topright[0], 1):
         for y in range(player_rect.y, player_rect.bottomleft[1], 1):
             try:
-                if screen.get_at((x, y)) == (220, 215, 177):
-                    reward = -100
-                    episode_end = True
+                if screen.get_at((x,y)) == (220, 215, 177):
                     return True
-            except:
+            except IndexError:
                 print("Oops")
     if hotel_rect.colliderect(player_rect):
-        reward = -100
-        episode_end = True
         return True
     return False
 
@@ -132,87 +100,122 @@ def draw_message(text, color):
     message = font.render(text, True, color)
     screen.blit(message, (350, 150))
     pg.display.flip()
-    pg.time.delay(1000)
+    pg.time.delay(4000)
+
+########################################################################
+
+actions = [0, 1, 2, 3]  # 0 - right, 1 - left, 2 - up, 3 - down
+
+Q_table = defaultdict(lambda: [0, 0, 0, 0])
+
+learning_rate = 0.9
+discount_factor = 0.9
+epsilon = 0.1
 
 
-def pickup():
-    if passenger_rect.colliderect(player_rect):
-        return True
-    return False
+def draw():
+    screen.fill(BLACK)
+    screen.blit(images_dict['bg'], (0, 0))
+    screen.blit(parking_img, parking_rect)
+    screen.blit(hotel_img, hotel_rect)
+    screen.blit(passenger_img, passenger_rect)
+    screen.blit(images_dict['player'][player_view], player_rect)
+    pg.display.flip()
 
-pg.init()
-screen = pg.display.set_mode([width, height])
+
+def choose_action(state):
+    if random.random() < epsilon:
+        return random.choice(actions)
+    else:
+        return np.argmax(Q_table[state])
+
+
+def update_q(state, action, reward, next_state):
+    best_next = max(Q_table[next_state])
+    Q_table[state][action] += learning_rate * (reward + discount_factor * best_next - Q_table[state][action])
+
+
+def make_step():
+    current_state = (player_rect.x, player_rect.y)
+    action = choose_action(current_state)
+    apply_action(action)
+    draw()
+    reward = -1
+    episode_end = False
+    success = False
+    if is_crash():
+        reward = -100
+        episode_end = True
+    if parking_rect.contains(player_rect):
+        reward = 100
+        episode_end = True
+        success = True
+    next_state = (player_rect.x, player_rect.y)
+    update_q(current_state, action, reward, next_state)
+    return (episode_end, success)
+
+
+########################################################################
+num_episodes = 300
+max_steps = 50
+
+for episode in range(num_episodes):
+    player_rect.x = 300
+    player_rect.y = 300
+    for step in range(max_steps):
+        (episode_end, success) = make_step()
+        if episode_end:
+            print("Win? -", success)
+            break
+print(Q_table)
+
+draw_message("End of learning", pg.Color("blue"))
+########################################################################
+
 timer = pg.time.Clock()
 
 run = True
 while run:
     timer.tick(FPS)
+    current_state = (player_rect.x, player_rect.y)
+    action = choose_action(current_state)
+    apply_action(action)
+
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
-
-    # keys_klava = pg.key.get_pressed()
-    # if keys_klava[pg.K_RIGHT] and player_rect.x < width - player_rect.width:
-    #     x_direction = 1
-    #     player_view = 'right'
-    # elif keys_klava[pg.K_LEFT] and player_rect.x > 0:
-    #     x_direction = -1
-    #     player_view = 'left'
-    # elif keys_klava[pg.K_UP] and player_rect.y > 0:
-    #     y_direction = -1
-    #     player_view = 'rear'
-    # elif keys_klava[pg.K_DOWN] and player_rect.y <= height - player_rect.height:
-    #     y_direction = 1
-    #     player_view = 'front'
-
     player_rect.x += player_speed * x_direction
     player_rect.y += player_speed * y_direction
     x_direction = 0
     y_direction = 0
-
     if is_crash():
         print("CRASH")
+        # run = False
         player_view = 'rear'
         player_rect.x = 300
         player_rect.y = 300
-        passenger_picked = False
-        passenger_rect.x, passenger_rect.y = pas_position
+        passenger_rect.x, passenger_rect.y = random.choice(hotel_positions)
+        passenger_rect.y += hotel_rect.height
         reward = -100
-        episode_end = True
+        episode_end = False
         continue
+
     if parking_rect.contains(player_rect):
-        passenger_rect.x = hotel_rect.x
-        passenger_rect.y = hotel_rect.y + hotel_rect.height
-        passenger_picked = False
-        screen.blit(passenger_img, passenger_rect)
-        draw_message("You won", pg.Color("green"))
+        draw_message("You won", pg.Color('green'))
         player_view = 'rear'
         player_rect.x = 300
         player_rect.y = 300
-        reward = 100
         success = True
+        reward = 100
+
         hotel_rect.x, hotel_rect.y = random.choice(hotel_positions)
         parking_rect.x, parking_rect.y = hotel_rect.x, hotel_rect.y + hotel_rect.height
         passenger_rect.x, passenger_rect.y = random.choice(hotel_positions)
-        while passenger_rect.x == hotel_rect.x and passenger_rect.y == hotel_rect.y:
-            passenger_rect.x, passenger_rect.y = random.choice(hotel_positions)
         passenger_rect.y += hotel_rect.height
-        pas_position = passenger_rect.x, passenger_rect.y
-    if passenger_picked:
+        episode_end = True
+        continue
+    if player_rect.colliderect(passenger_rect):
         passenger_rect.x, passenger_rect.y = player_rect.x, player_rect.y
-    if not passenger_picked:
-        passenger_picked = pickup()
-
-
-    screen.fill(BLACK)
-    screen.blit(images_dict['bg'], (0, 0))
-
-    screen.blit(parking_img, parking_rect)
-    screen.blit(hotel_img, hotel_rect)
-    if not passenger_picked:
-        screen.blit(passenger_img, passenger_rect)
-    screen.blit(images_dict['player'][player_view], player_rect)
-
-    pg.display.flip()
+    draw()
 
 pg.quit()
