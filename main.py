4086de3e3801aba6e71967728a63c7be50d97e06
tata0409 <tatachechyna@gmail.com>
2025-04-04 21:09:34 -0400

    Cleaned up the code

--- main.py
+++ main.py
@@ -4,51 +4,19 @@ import random
 width, height = 700, 450
 FPS = 60
 BLACK = (0, 0, 0)
-x_direction = 0
-y_direction = 0
-player_speed = 2
-images_dict = {
-    'bg': pg.image.load('img/Background.png'),
-    'player': {
-        'rear': pg.image.load('img/cab_rear.png'),
-        'left': pg.image.load('img/cab_left.png'),
-        'front': pg.image.load('img/cab_front.png'),
-        'right': pg.image.load('img/cab_right.png'),
-    },
-    'hotel': pg.transform.scale(pg.image.load('img/hotel.png'), (80, 80)),
-    'passenger': pg.image.load('img/passenger.png'),
-    'hole': pg.image.load('img/hole.png'),
-    'taxi_background': pg.transform.scale(pg.image.load('img/taxi_background.png'), (80, 45)),
-    'parking': pg.transform.scale(pg.image.load('img/parking.png'), (80, 70))
-}
 
-player_view = 'rear'
-player_rect = images_dict['player'][player_view].get_rect()
-player_rect.x = 300
-player_rect.y = 300
 
-hotel_img = images_dict['hotel']
-hotel_rect = hotel_img.get_rect()
-hotel_positions = [
-    (60, 30),
-    (555, 30),
-    (60, 250),
-    (440, 250)
-]
-hotel_rect.x, hotel_rect.y = random.choice(hotel_positions)
-
-passenger_img = images_dict['passenger']
-passenger_rect = passenger_img.get_rect()
-passenger_rect.x, passenger_rect.y = random.choice(hotel_positions)
-while passenger_rect.x == hotel_rect.x and passenger_rect.y == hotel_rect.y:
+def start_positions():
+    global player_view
+    player_view = 'rear'
+    player_rect.x = 300
+    player_rect.y = 300
+    hotel_rect.x, hotel_rect.y = random.choice(hotel_positions)
+    parking_rect.x, parking_rect.y = hotel_rect.x, hotel_rect.y + hotel_rect.height
     passenger_rect.x, passenger_rect.y = random.choice(hotel_positions)
-passenger_rect.y += hotel_rect.height
-pas_position = passenger_rect.x, passenger_rect.y
-passenger_picked = False
-
-parking_img = images_dict['parking']
-parking_rect = parking_img.get_rect()
-parking_rect.x, parking_rect.y = hotel_rect.x, hotel_rect.y + hotel_rect.height
+    while passenger_rect.x == hotel_rect.x and passenger_rect.y == hotel_rect.y:
+        passenger_rect.x, passenger_rect.y = random.choice(hotel_positions)
+    passenger_rect.y += hotel_rect.height
 
 
 def is_crash():
@@ -77,9 +45,49 @@ def pickup():
         return True
     return False
 
+
+x_direction = 0
+y_direction = 0
+player_speed = 2
+images_dict = {
+    'bg': pg.image.load('img/Background.png'),
+    'player': {
+        'rear': pg.image.load('img/cab_rear.png'),
+        'left': pg.image.load('img/cab_left.png'),
+        'front': pg.image.load('img/cab_front.png'),
+        'right': pg.image.load('img/cab_right.png'),
+    },
+    'hotel': pg.transform.scale(pg.image.load('img/hotel.png'), (80, 80)),
+    'passenger': pg.image.load('img/passenger.png'),
+    'hole': pg.image.load('img/hole.png'),
+    'taxi_background': pg.transform.scale(pg.image.load('img/taxi_background.png'), (80, 45)),
+    'parking': pg.transform.scale(pg.image.load('img/parking.png'), (80, 70))
+}
+
+player_view = 'rear'
+player_rect = images_dict['player'][player_view].get_rect()
+
+
+hotel_img = images_dict['hotel']
+hotel_rect = hotel_img.get_rect()
+hotel_positions = [
+    (60, 30),
+    (555, 30),
+    (60, 250),
+    (440, 250)
+]
+
+passenger_img = images_dict['passenger']
+passenger_rect = passenger_img.get_rect()
+
+parking_img = images_dict['parking']
+parking_rect = parking_img.get_rect()
+
+
 pg.init()
 screen = pg.display.set_mode([width, height])
 timer = pg.time.Clock()
+start_positions()
 
 run = True
 while run:
@@ -109,11 +117,7 @@ while run:
 
     if is_crash():
         print("CRASH")
-        player_view = 'rear'
-        player_rect.x = 300
-        player_rect.y = 300
-        passenger_picked = False
-        passenger_rect.x, passenger_rect.y = pas_position
+        start_positions()
         continue
     if parking_rect.contains(player_rect):
         passenger_rect.x = hotel_rect.x
@@ -121,16 +125,7 @@ while run:
         passenger_picked = False
         screen.blit(passenger_img, passenger_rect)
         draw_message("You won", pg.Color("green"))
-        player_view = 'rear'
-        player_rect.x = 300
-        player_rect.y = 300
-        hotel_rect.x, hotel_rect.y = random.choice(hotel_positions)
-        parking_rect.x, parking_rect.y = hotel_rect.x, hotel_rect.y + hotel_rect.height
-        passenger_rect.x, passenger_rect.y = random.choice(hotel_positions)
-        while passenger_rect.x == hotel_rect.x and passenger_rect.y == hotel_rect.y:
-            passenger_rect.x, passenger_rect.y = random.choice(hotel_positions)
-        passenger_rect.y += hotel_rect.height
-        pas_position = passenger_rect.x, passenger_rect.y
+        start_positions()
     if passenger_picked:
         passenger_rect.x, passenger_rect.y = player_rect.x, player_rect.y
     if not passenger_picked:
--- main_ML.py
+++ main_ML.py
@@ -8,54 +8,45 @@ FPS = 60
 
 BLACK = (0, 0, 0)
 
-x_direction = 0
-y_direction = 0
-player_speed = 2
-images_dict = {
-    'bg': pg.image.load('img/Background.png'),
-    'player': {
-        'rear': pg.image.load('img/cab_rear.png'),
-        'left': pg.image.load('img/cab_left.png'),
-        'front': pg.image.load('img/cab_front.png'),
-        'right': pg.image.load('img/cab_right.png'),
-    },
-    'hotel': pg.transform.scale(pg.image.load('img/hotel.png'), (80, 80)),
-    'passenger': pg.image.load('img/passenger.png'),
-    'hole': pg.image.load('img/hole.png'),
-    'taxi_background': pg.transform.scale(pg.image.load('img/taxi_background.png'), (80, 45)),
-    'parking': pg.transform.scale(pg.image.load('img/parking.png'), (80, 70))
-}
 
-player_view = 'rear'
-player_rect = images_dict['player'][player_view].get_rect()
-player_rect.x = 300
-player_rect.y = 300
-
-hotel_img = images_dict['hotel']
-hotel_rect = hotel_img.get_rect()
-hotel_positions = [
-    (60, 30),
-    (555, 30),
-    (60, 250),
-    (440, 250)
-]
-hotel_rect.x, hotel_rect.y = random.choice(hotel_positions)
-
-
-parking_img = images_dict['parking']
-parking_rect = parking_img.get_rect()
-parking_rect.x, parking_rect.y = hotel_rect.x, hotel_rect.y + hotel_rect.height
-
-
-passenger_img = images_dict['passenger']
-passenger_rect = passenger_img.get_rect()
-passenger_rect.x, passenger_rect.y = random.choice(hotel_positions)
-while passenger_rect.x == hotel_rect.x and passenger_rect.y == hotel_rect.y:
+def start_positions():
+    global player_view
+    player_view = 'rear'
+    player_rect.x = 300
+    player_rect.y = 300
+    hotel_rect.x, hotel_rect.y = random.choice(hotel_positions)
+    parking_rect.x, parking_rect.y = hotel_rect.x, hotel_rect.y + hotel_rect.height
     passenger_rect.x, passenger_rect.y = random.choice(hotel_positions)
-passenger_rect.y += hotel_rect.height
+    while passenger_rect.x == hotel_rect.x and passenger_rect.y == hotel_rect.y:
+        passenger_rect.x, passenger_rect.y = random.choice(hotel_positions)
+    passenger_rect.y += hotel_rect.height
 
-pg.init()
-screen = pg.display.set_mode([width, height])
+
+def draw():
+    if learned:
+        pg.time.delay(100)
+    screen.fill(BLACK)
+    screen.blit(images_dict['bg'], (0, 0))
+    screen.blit(parking_img, parking_rect)
+    screen.blit(hotel_img, hotel_rect)
+    screen.blit(passenger_img, passenger_rect)
+    screen.blit(images_dict['player'][player_view], player_rect)
+    pg.display.flip()
+
+
+def is_crash():
+    for x in range(player_rect.x, player_rect.topright[0], 1):
+        for y in range(player_rect.y, player_rect.bottomleft[1], 1):
+            try:
+                if screen.get_at((x, y)) == (220, 215, 177):
+                    start_positions()
+                    return True
+            except IndexError:
+                print("Oops")
+    if hotel_rect.colliderect(player_rect):
+        start_positions()
+        return True
+    return False
 
 
 def apply_action(action):
@@ -82,18 +73,6 @@ def apply_action(action):
         player_rect.x, player_rect.y = new_x, new_y
 
 
-def is_crash():
-    for x in range(player_rect.x, player_rect.topright[0], 1):
-        for y in range(player_rect.y, player_rect.bottomleft[1], 1):
-            try:
-                if screen.get_at((x,y)) == (220, 215, 177):
-                    return True
-            except IndexError:
-                print("Oops")
-    if hotel_rect.colliderect(player_rect):
-        return True
-    return False
-
 
 def draw_message(text, color):
     font = pg.font.SysFont(None, 36)
@@ -101,6 +80,45 @@ def draw_message(text, color):
     screen.blit(message, (350, 150))
     pg.display.flip()
     pg.time.delay(4000)
+
+
+images_dict = {
+    'bg': pg.image.load('img/Background.png'),
+    'player': {
+        'rear': pg.image.load('img/cab_rear.png'),
+        'left': pg.image.load('img/cab_left.png'),
+        'front': pg.image.load('img/cab_front.png'),
+        'right': pg.image.load('img/cab_right.png'),
+    },
+    'hotel': pg.transform.scale(pg.image.load('img/hotel.png'), (80, 80)),
+    'passenger': pg.image.load('img/passenger.png'),
+    'hole': pg.image.load('img/hole.png'),
+    'taxi_background': pg.transform.scale(pg.image.load('img/taxi_background.png'), (80, 45)),
+    'parking': pg.transform.scale(pg.image.load('img/parking.png'), (80, 70))
+}
+
+player_view = 'rear'
+player_rect = images_dict['player'][player_view].get_rect()
+
+hotel_img = images_dict['hotel']
+hotel_rect = hotel_img.get_rect()
+hotel_positions = [
+    (60, 30),
+    (555, 30),
+    (60, 250),
+    (440, 250)
+]
+
+parking_img = images_dict['parking']
+parking_rect = parking_img.get_rect()
+
+
+passenger_img = images_dict['passenger']
+passenger_rect = passenger_img.get_rect()
+
+
+pg.init()
+screen = pg.display.set_mode([width, height])
 
 ########################################################################
 
@@ -111,17 +129,6 @@ Q_table = defaultdict(lambda: [0, 0, 0, 0])
 learning_rate = 0.9
 discount_factor = 0.9
 epsilon = 0.1
-
-
-def draw():
-    screen.fill(BLACK)
-    screen.blit(images_dict['bg'], (0, 0))
-    screen.blit(parking_img, parking_rect)
-    screen.blit(hotel_img, hotel_rect)
-    screen.blit(passenger_img, passenger_rect)
-    screen.blit(images_dict['player'][player_view], player_rect)
-    pg.display.flip()
-
 
 def choose_action(state):
     if random.random() < epsilon:
@@ -155,8 +162,14 @@ def make_step():
     return (episode_end, success)
 
 
-########################################################################
-num_episodes = 300
+pg.init()
+screen = pg.display.set_mode([width, height])
+timer = pg.time.Clock()
+start_positions()
+learned = False
+draw()
+
+num_episodes = 3000
 max_steps = 50
 
 for episode in range(num_episodes):
@@ -167,6 +180,8 @@ for episode in range(num_episodes):
         if episode_end:
             print("Win? -", success)
             break
+
+learned = True
 print(Q_table)
 
 draw_message("End of learning", pg.Color("blue"))
@@ -184,34 +199,23 @@ while run:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
-    player_rect.x += player_speed * x_direction
-    player_rect.y += player_speed * y_direction
-    x_direction = 0
-    y_direction = 0
+    current_state = (player_rect.x, player_rect.y)
+    action = choose_action(current_state)
+    apply_action(action)
+
     if is_crash():
         print("CRASH")
         # run = False
-        player_view = 'rear'
-        player_rect.x = 300
-        player_rect.y = 300
-        passenger_rect.x, passenger_rect.y = random.choice(hotel_positions)
-        passenger_rect.y += hotel_rect.height
+        start_positions()
         reward = -100
         episode_end = False
         continue
 
     if parking_rect.contains(player_rect):
         draw_message("You won", pg.Color('green'))
-        player_view = 'rear'
-        player_rect.x = 300
-        player_rect.y = 300
+        start_positions()
         success = True
         reward = 100
-
-        hotel_rect.x, hotel_rect.y = random.choice(hotel_positions)
-        parking_rect.x, parking_rect.y = hotel_rect.x, hotel_rect.y + hotel_rect.height
-        passenger_rect.x, passenger_rect.y = random.choice(hotel_positions)
-        passenger_rect.y += hotel_rect.height
         episode_end = True
         continue
     if player_rect.colliderect(passenger_rect):
